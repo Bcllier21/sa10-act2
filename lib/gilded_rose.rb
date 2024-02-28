633ef8aec8e84272c3c5421b5a12a2c1ac36196e
Brian Collier <briancollier358@gmail.com>
2024-02-28 00:14:39 -0600

    Wrote method 'normal_tick' to pass tests concerning Normal Item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,49 +7,61 @@ class GildedRose
     @quality = quality
   end
 
-  def tick
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
+  def normal_tick
+    if @quality != 0
+      if @days_remaining > 0
+        @quality -= 1
       end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
+      if @days_remaining <= 0
+        @quality -= 2
       end
     end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
+    @days_remaining -= 1
+  end
+
+    def tick
+      if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
+        if @quality > 0
+          if @name != "Sulfuras, Hand of Ragnaros"
+            @quality = @quality - 1
           end
-        else
-          @quality = @quality - @quality
         end
       else
         if @quality < 50
           @quality = @quality + 1
+          if @name == "Backstage passes to a TAFKAL80ETC concert"
+            if @days_remaining < 11
+              if @quality < 50
+                @quality = @quality + 1
+              end
+            end
+            if @days_remaining < 6
+              if @quality < 50
+                @quality = @quality + 1
+              end
+            end
+          end
+        end
+      end
+      if @name != "Sulfuras, Hand of Ragnaros"
+        @days_remaining = @days_remaining - 1
+      end
+      if @days_remaining < 0
+        if @name != "Aged Brie"
+          if @name != "Backstage passes to a TAFKAL80ETC concert"
+            if @quality > 0
+              if @name != "Sulfuras, Hand of Ragnaros"
+                @quality = @quality - 1
+              end
+            end
+          else
+            @quality = @quality - @quality
+          end
+        else
+          if @quality < 50
+            @quality = @quality + 1
+          end
         end
       end
     end
   end
-end
