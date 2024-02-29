51f5223eada4e41fb17b6f7cfb23f8effea41f6d
Brian Collier <briancollier358@gmail.com>
2024-02-28 23:01:09 -0600

    Finished GildedRose Kata

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,24 +1,11 @@
 module GildedRose
   class Item
-    attr_reader :days_remaining, :quality
-    def initialize(days_remaining, quality)
-      @days_remaining, @quality = days_remaining, quality
-    end
-  end
-  def self.new(name:, days_remaining:, quality:)
-    klass_for(name).new(days_remaining, quality)
-  end
+    attr_reader :quality, :days_remaining
 
-  def self.klass_for(name)
-    case name
-    when 'Normal Item'
-      Normal_Item
-    when 'Aged Brie'
-      Brie
-    when 'Sulfuras, Hand of Ragnaros'
-      Sulfuras
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      Backstage
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
     end
   end
 
@@ -42,11 +29,6 @@ module GildedRose
     end
   end
 
-  class Sulfuras < Item
-    def tick
-    end
-  end
-
   class Backstage < Item
     def tick
       @days_remaining -= 1
@@ -57,5 +39,26 @@ module GildedRose
       @quality += 1 if @days_remaining < 10
       @quality += 1 if @days_remaining < 5
     end
+  end
+
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'Normal Item'                               => Normal_Item,
+    'Aged Brie'                                 => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    'Conjured Mana Cake'                        => Conjured }
+  
+  def self.new(name:, quality:, days_remaining:)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
   end
 end
