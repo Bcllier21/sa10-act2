7ed865baa0effae2cd78b5f062f9764655dfa46f
Brian Collier <briancollier358@gmail.com>
2024-02-28 20:08:25 -0600

    Reworked magic strings into classes with their own 'tick' methods, cases now call and forward these classes, 'days_remaining' and 'quality' use methods for fetching (temporary).

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,46 +7,91 @@ class GildedRose
     @quality = quality
   end
 
-    def tick
-      case name
-      when 'Normal Item'
-        return normal_tick
-      when 'Aged Brie'
-        return brie_tick
-      when 'Sulfuras, Hand of Ragnaros'
-        return sulfuras_tick
-      when 'Backstage passes to a TAFKAL80ETC concert'
-        return backstage_tick
-      end
+  def tick
+    case name
+    when 'Normal Item'
+      @item = Normal.new(quality, days_remaining)
+      item.tick
+    when 'Aged Brie'
+      @item = Brie.new(quality, days_remaining)
+      item.tick
+    when 'Sulfuras, Hand of Ragnaros'
+      @item = Sulfuras.new(quality, days_remaining)
+      item.tick
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      @item = Backstage.new(quality, days_remaining)
+      item.tick
+    end
+  end
+
+  def quality
+    return item.quality if item
+    @quality
+  end
+
+  def days_remaining
+    return item.days_remaining if item
+    @days_remaining
+  end
+
+  class Normal
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
     end
 
-    def normal_tick
+    def tick
       @days_remaining -= 1
       return if @quality == 0
 
       @quality -= 1
       @quality -= 1 if @days_remaining <= 0
     end
+  end
 
-    def brie_tick
+  class Brie
+    attr_reader :quality, :days_remaining
+
+    def initialize
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
       @days_remaining -= 1
       return if @quality >= 50
-
+  
       @quality += 1 
       @quality += 1 if @days_remaining <= 0 && @quality < 50
     end
+  end
 
-    def sulfuras_tick
+  class Sulfuras
+    attr_reader :quality, :days_remaining
 
+    def initialize
+      @quality, @days_remaining = quality, days_remaining
     end
 
-    def backstage_tick
+    def tick
+    end
+  end
+
+  class Backstage
+    attr_reader :quality, :days_remaining
+
+    def initialize
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
       @days_remaining -= 1
       return if quality >= 50
       return @quality = 0 if @days_remaining < 0
-
+  
       @quality += 1
       @quality += 1 if @days_remaining < 10
       @quality += 1 if @days_remaining < 5
     end
   end
+end
